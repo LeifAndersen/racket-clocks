c91eb3e25ec8538056c866de0ea7c35481987c9b
Leif Andersen <leif@leifandersen.net>
2014-10-29 13:55:15 -0400

    Test size of clock_t type.

---
+++ ffi-test/clock-t-size.c
@@ -0,0 +1,9 @@
+#include <time.h>
+#include <stdio.h>
+
+int main()
+{
+  printf("UInt: %d\n", sizeof(unsigned int));
+  printf("ULong: %d\n", sizeof(unsigned long));
+  printf("Clock_t: %d\n", sizeof(clock_t));
+}
